d770d2fab780add0feaf807016ff273405af313f
Divyam Garg <dvm.garg26@gmail.com>
2023-08-14 15:06:09 +0530

    changes

---
+++ pages/q1.js
@@ -0,0 +1,66 @@
+let response = {
+    "success": true,
+    "survey_name": "Quiz Name",
+    "survey_description": "This is a user-side template of the quiz with the required features such as switching b/w the questions, keeping the track of attempted questions, time bounded feature",
+    "quiz_instructions": "",
+    "survey_start_date": date,
+    "already_answered": false,
+    "quiz_duration": 3600,
+    "survey_question_details": [
+        {
+            "question_id": 53,
+            "statement": "Would you recommend [organization] to your friends as an employer?",
+            "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+            "is_mandatory": true,
+            "ques_type": "MCQ",
+            "weight": 1,
+            "extras": {
+                "max_lenght": null,
+                "min_length": null,
+                "rating_length": null
+            },
+            "options_attributes": [
+                {
+                    "id": 92,
+                    "survey_question_id": 53,
+                    "statement": "Absolutely!",
+                    "placeholder": null,
+                    "option_type": null,
+                    "extra": null,
+                    "created_at": "2023-05-16T06:17:11.000Z",
+                    "updated_at": "2023-05-16T06:17:11.000Z"
+                },
+                {
+                    "id": 93,
+                    "survey_question_id": 53,
+                    "statement": "Maybe",
+                    "placeholder": null,
+                    "option_type": null,
+                    "extra": null,
+                    "created_at": "2023-05-16T06:17:11.001Z",
+                    "updated_at": "2023-05-16T06:17:11.001Z"
+                },
+                {
+                    "id": 94,
+                    "survey_question_id": 53,
+                    "statement": "Not very Likely",
+                    "placeholder": null,
+                    "option_type": null,
+                    "extra": null,
+                    "created_at": "2023-05-16T06:17:11.003Z",
+                    "updated_at": "2023-05-16T06:17:11.003Z"
+                },
+                {
+                    "id": 95,
+                    "survey_question_id": 53,
+                    "statement": "Never",
+                    "placeholder": null,
+                    "option_type": null,
+                    "extra": null,
+                    "created_at": "2023-05-16T06:17:11.003Z",
+                    "updated_at": "2023-05-16T06:17:11.003Z"
+                }
+            ]
+        },
+    ]
+}
